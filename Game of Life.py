1a2cd26b9268802b1332423a02aeff8d87ef452e
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2022-09-23 17:08:14 +0100

    Fixed bug with animation

--- Game of Life.py
+++ Game of Life.py
@@ -2,26 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-GRID_DIMENSION = 10
-FRAMES = 10
-UPDATE_INTERVAL = 50
-SAVE_COUNT = 50
+GRID_DIMENSION = 100
+FRAMES = 100
+UPDATE_INTERVAL = 1000/60
 
 
-def cycle(frame, img, old_grid):
-    new_grid = np.zeros((10, 10))
+def cycle(frame_number, img, old_grid):
+    new_grid = old_grid.copy()
     for i in range(GRID_DIMENSION):
         for j in range(GRID_DIMENSION):
             nbr = neighbours(old_grid, i, j)
             if old_grid[i][j] == 1 and (nbr < 2 or nbr > 3):
                 new_grid[i][j] = 0
-            elif old_grid[i][j] == 0 or nbr == 3:
+            elif old_grid[i][j] == 0 and nbr == 3:
                 new_grid[i][j] = 1
-            else:
-                new_grid[i][j] = old_grid[i][j]
     img.set_data(new_grid)
-    old_grid = new_grid[:]
-    return img,
+    old_grid[:] = new_grid[:]
+    return img
 
 
 def neighbours(old_grid, x, y):
@@ -30,17 +27,18 @@ def neighbours(old_grid, x, y):
         for j in (-1, 0, +1):
             X = (x + i + GRID_DIMENSION) % GRID_DIMENSION
             Y = (y + j + GRID_DIMENSION) % GRID_DIMENSION
-            if X != Y:
-                neighbour_sum += old_grid[X][Y]
+            neighbour_sum += old_grid[X][Y]
+    neighbour_sum -= old_grid[x][y]
     return neighbour_sum
 
 
 def main():
     grid = np.floor(np.random.rand(GRID_DIMENSION, GRID_DIMENSION) * 2)
     figure, axes = plt.subplots()
+    plt.title("Conway's Game of Life")
     image = axes.imshow(grid, interpolation="nearest")
-    anim = FuncAnimation(figure, cycle, fargs=(image, grid))
-    anim.save("game_of_life", fps=30, extra_args=['-vcodec', 'libx264'])
+    anim = FuncAnimation(figure, cycle, fargs=(image, grid), frames=FRAMES,
+                         interval=UPDATE_INTERVAL)
     plt.show()
 
 
